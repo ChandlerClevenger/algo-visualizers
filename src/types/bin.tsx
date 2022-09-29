fd4b85ff0e301d4a3431a5c487ca568a42fcf2ee
Chandler Clevenger <Chandler888@icloud.com>
2022-09-28 22:40:14 -0400

    add Graph types and visual interfaces

---
+++ src/types/bin.tsx
@@ -0,0 +1,35 @@
+export interface RouterInt extends Node {
+  onStop: any;
+  start: any;
+  onDrag: any;
+  x: number;
+  y: number;
+  size: number;
+}
+
+export interface LinePos {
+  id: number;
+  x1: number;
+  x2: number;
+  y1: number;
+  y2: number;
+}
+
+export interface Node {
+  id: number;
+  prevNode: Node | null;
+  nextNode: Node | null;
+  weight: number;
+}
+
+export interface Edge {
+  id: number;
+  firstNode: Node;
+  secondNode: Node;
+  weight: number;
+}
+
+export interface Graph {
+  nodes: Node[];
+  edges: Edge[];
+}
