5a289fcbf0cefe4bc42b74af529313fd133e9b51
Chandler Clevenger <Chandler888@icloud.com>
2022-09-30 14:50:17 -0400

    add local logic to routers

--- src/components/Routers.tsx
+++ src/components/Routers.tsx
@@ -1,40 +1,92 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { DraggableData } from "react-draggable";
-import { Edge, RouterInt } from "../types/bin";
+import { Node, NodePos, RouterInt } from "../types/bin";
 import Router from "./DraggableRouter";
+import { uuid } from "uuidv4";
+const ROUTER_SIZE = 75;
+const LINE_OFFSET = ROUTER_SIZE / 2;
 
-export default function Routers({ sendUpEdges }: any) {
-  const ROUTER_SIZE = 75;
-  const LINE_OFFSET = ROUTER_SIZE / 2;
+export default function Routers({
+  sendUpNodes,
+  sendUpClickedNodeData,
+  sendUpNodePos,
+}: any) {
   const INITIAL_ROUTER: RouterInt = {
-    onStop: drop,
-    start: start,
-    onDrag: drag,
     id: 0,
-    x: 0,
-    y: 0,
+    onDrag: drag,
+    onStart: start,
+    onStop: stop,
     prevNode: null,
     nextNode: null,
     size: ROUTER_SIZE,
-    weight: -1,
+    weight: Infinity,
   };
+
   const [routers, setRouters] = useState<RouterInt[]>([INITIAL_ROUTER]);
-  function updateEdges(edges: Edge[]): void {
-    sendUpEdges(edges);
+  let currentDraggedRef = useRef({ x: 0, y: 0 });
+
+  useEffect(() => {
+    sendUpNodes([...routers]);
+  }, [routers]);
+
+  function stop(e: MouseEvent, info: DraggableData) {
+    const DRAGGED_ID = Number(info.node.id);
+    const { x, y } = { ...info };
+    // Must move at least 100 px out of start area
+    if (x < 100 && y < 100) return;
+    // Detect click
+    if (currentDraggedRef.current.x == x && currentDraggedRef.current.y == y) {
+      sendUpClickedNodeData(
+        routers.find((r) => {
+          return r.id == DRAGGED_ID;
+        }),
+        { id: DRAGGED_ID, x: info.x, y: info.y }
+      );
+      return;
+    }
+    if (DRAGGED_ID == routers.length - 1) {
+      setRouters((oldR) => [
+        ...oldR,
+        {
+          id: routers.length,
+          onStart: start,
+          onStop: stop,
+          onDrag: drag,
+          prevNode: null,
+          nextNode: null,
+          size: ROUTER_SIZE,
+          weight: Infinity,
+        },
+      ]);
+    }
   }
 
-  function drag(e: MouseEvent, info: DraggableData): void {}
+  function start(e: MouseEvent, info: DraggableData) {
+    currentDraggedRef.current = { x: info.x, y: info.y };
+  }
 
-  // Set draggables current position to check if click or drag
-  // This is checked in the drop function
-  function start(e: MouseEvent, info: DraggableData): void {}
+  function drag(e: MouseEvent, info: DraggableData) {
+    sendUpNodePos({ id: Number(info.node.id), x: info.x, y: info.y });
+  }
 
-  function drop(e: MouseEvent, info: DraggableData): void {}
+  function generateId(uid: string): number {
+    return parseInt(uid.replace("-", ""), 16);
+  }
 
   return (
     <>
       {routers.map((router, index) => (
-        <Router key={index} {...router}></Router>
+        <Router
+          key={index}
+          id={router.id}
+          onStart={start}
+          onStop={stop}
+          onDrag={drag}
+          size={router.size}
+          nextNode={router.nextNode}
+          prevNode={router.prevNode}
+          weight={router.weight}
+        ></Router>
       ))}
     </>
   );
