45565f893a0041d1257759f73dd6b528446f53c1
Chandler Clevenger <Chandler888@icloud.com>
2022-10-03 21:17:56 -0400

    add functionality to change line weight

--- src/components/Lines.tsx
+++ src/components/Lines.tsx
@@ -1,13 +1,29 @@
-import { LinePos } from "../types/bin";
+import $ from "jquery";
+import regex from "../utils/my_regex";
+import { LineData } from "../types/bin";
 import Line from "./Line";
 
-export default function Lines({ linePositions }: any) {
+const my_regex = new regex();
+export default function Lines({ linePositions, onWeightChange }: any) {
+  function clicked(e: React.MouseEvent<SVGLineElement | SVGTextElement>): void {
+    const w = prompt("Enter a number: ");
+    if (!w || (w && isNaN(Number(w)))) {
+      throw TypeError("NAN or nothing entered.");
+    }
+    const id = $(e.target).attr("id");
+    if (!id) {
+      throw TypeError("Failure to get id of line");
+    }
+    onWeightChange(Number(my_regex.getNumbersOnly(id)), Number(w));
+  }
+
   return (
     <svg id="lines" className="position-absolute w-100 h-100">
-      {linePositions.map((linePos: LinePos, index: number) => (
+      {linePositions.map((linePos: LineData, index: number) => (
         <Line
           key={index}
           id={linePos.id}
+          clicked={clicked}
           firstConnector={linePos.firstConnector}
           secondConnector={linePos.secondConnector}
           weight={linePos.weight}
