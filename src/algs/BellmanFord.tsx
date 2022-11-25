6559eb3c6e430f07ea2cac31228b18d1ab95974a
Chandler Clevenger <Chandler888@icloud.com>
2022-11-25 18:12:01 -0500

    Implement table optimization

--- src/algs/BellmanFord.tsx
+++ src/algs/BellmanFord.tsx
@@ -10,62 +10,61 @@ export default class BellmanFord {
         let routers = this.#initilizeRouters(edges, nodes);
 
         let hasChanges = true; // True initially so while loop runs
-        while(hasChanges) {
-            console.log("Looping")
+        while (hasChanges) {
             hasChanges = false; // Easier to set this false each loop since it starts true
             // Loop through all Routers and share table with neighbors that are not next hop on weight value
             // Set hasChanged to true if any tables are updated
             for (const router of routers){
                 // Get routers connections
                 const conns = this.#getConnections(router.id, edges);
-                console.log("Conns:", conns)
                 for (const con of conns) {
-                    [routers, hasChanges] = this.#optimizeTable(con, routers)
+                    let localHasChanged = false;
+                    [routers, localHasChanged] = this.#optimizeTable(con, routers)
+                    hasChanges = (localHasChanged || hasChanges ) // Keeps true if any localHasChanged return true once
                 }
-            } 
+            }
         }
         console.log(routers)
     }
 
     #optimizeTable(connection: IConnection, routers: IRouter[]): [IRouter[], boolean] {
-        const otherRouter = routers.find(r => connection.otherRouterId === r.id); // Router to optimize 
-        const currentRouter = routers.find(r => connection.selfRouterId === r.id);
-        if (! otherRouter || ! currentRouter) throw new Error("Errr")
+        const toRouter = routers.find(r => connection.otherRouterId === r.id); // Router to optimize 
+        const fromRouter = routers.find(r => connection.selfRouterId === r.id);
+        if (!toRouter || !fromRouter) throw new Error("Error, check connections.")
         let hasChanged = false;
-        for (const [key, value] of currentRouter.table.entries()) {
-            if (value.nextHop === otherRouter.id) continue; // Dont 'send' optimize route to nextHop already in path
-            if (!otherRouter.table.has(key)) { // If not set routerId then add newly found Router
-                console.log("Setting, ", key, "\n of ", currentRouter.id)
-                otherRouter.table.set((key), {
-                    nextHop: currentRouter.id,
-                    destination: key,
-                    distance: connection.weight + value.distance
-                })
-                hasChanged = true;
-            }
-            let selfRouterCon = currentRouter.table.get(key);
+        for (const [routerId, distanceInfo] of fromRouter.table.entries()) {
+            if (distanceInfo.nextHop === toRouter.id) continue; // Dont 'send' optimize route to nextHop already in path
+
+            let selfRouterCon = fromRouter.table.get(routerId);
             if (!selfRouterCon) throw new Error("No selfRouter eerr");
             const selfRouterDistance = selfRouterCon?.distance;
-            
-            if (connection.weight + value.distance < selfRouterDistance) { // If distance is shorter, add new path & dist
-                currentRouter.table.set(key, {
-                    nextHop: otherRouter.id,
-                    destination: selfRouterCon.destination,
-                    distance: connection.weight + value.distance
+
+            if (!toRouter.table.has(routerId)) { // If not set routerId then add newly found Router
+                toRouter.table.set((routerId), {
+                    nextHop: fromRouter.id,
+                    destination: routerId,
+                    distance: connection.weight + distanceInfo.distance
+                })
+                hasChanged = true;
+            } // Otherwise, If distance is shorter, add new path & dist
+            else if (toRouter.table.has(routerId) && connection.weight + distanceInfo.distance < selfRouterDistance) { 
+                toRouter.table.set(routerId, {
+                    nextHop: fromRouter.id,
+                    destination: routerId,
+                    distance: connection.weight + distanceInfo.distance
                 })
                 hasChanged = true;
             }
           }
-
-          // Remember to replace selfRouter before return
-          routers.map(r => { // Replace 
-            if (r.id === currentRouter.id) {
-                return currentRouter;
-            }
-            else {
-                return r;
-            }
-          })
+            // Remember to replace selfRouter before return
+            routers.map(r => { // Replace router with new state
+                if (r.id === toRouter.id) {
+                    return toRouter;
+                }
+                else {
+                    return r;
+                }
+              })
           return [routers, hasChanged]
     }
 
@@ -99,6 +98,7 @@ export default class BellmanFord {
         // Populate table based on connections
         for (const conn of connections) {
             table.set(conn.otherRouterId, {
+                destination: conn.otherRouterId,
                 distance: conn.weight,
                 nextHop: conn.otherRouterId
             } as IDistance)
